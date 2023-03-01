0eb15cbef72609e51a99f9148353a0df53ff00a6
Igor Cacerez <igor.cacerez@gmail.com>
2023-02-28 22:54:04 -0300

    Jogo pong

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# jogo-pong-pong-python
-Jogo Pong Pong foi em Python, utilizando o Pygame. 
+# Jogo Pong Pongo
+
+Jogo simples feito em Python utilizando a biblioteca PyGame. 
+
+# Para usar
+
+Tendo o python instalado em seu computado basta rodar o comando no terminal, **python main.py**, dentro da pasta do projeto.
+
+
+## Demonstração
+
+![sesi](https://user-images.githubusercontent.com/29128449/210409646-ff2d4de0-2ccd-4a14-800a-40c4fc74ac27.gif)
---
+++ main.py
@@ -0,0 +1,139 @@
+import pygame
+
+pygame.init()
+pygame.font.init()
+
+display = pygame.display.set_mode((1280,720))
+
+p1_img = pygame.image.load("assets/user-1.png")
+player1 = p1_img.get_rect()
+player1_score = 0
+player1_speed = 7
+
+p2_img = pygame.image.load("assets/user-2.png")
+player2 = p2_img.get_rect(right=1280)
+player2_score = 0
+
+ball_img = pygame.image.load("assets/bola.png")
+ball = ball_img.get_rect(center=[1280 / 2, 720 /2])
+ball_dir_x = 10
+ball_dir_y = 10
+
+font = pygame.font.Font(None, 50)
+placar_player1 = font.render(str(player1_score), True, "white")
+placar_player2 = font.render(str(player2_score), True, "white")
+
+campo_img = pygame.image.load("assets/campo.png")
+campo = campo_img.get_rect()
+
+cena = "menu"
+
+fps = pygame.time.Clock()
+
+fade_img = pygame.Surface((1280, 720)).convert_alpha()
+fade_img.fill("black")
+fade_alpha = 255
+
+music = pygame.mixer.Sound("assets/music.ogg")
+music.play(-1)
+
+loop = True
+while loop:
+    
+    if cena == "jogo":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    loop = False
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w or event.key == pygame.K_s:
+                    player1_speed *= -1 
+        
+        if player2_score >= 3:
+           cena = "gameover"
+
+        if ball.colliderect(player1) or ball.colliderect(player2):
+            ball_dir_x *= -1
+            hit = pygame.mixer.Sound("assets/pong.wav")
+            hit.play()
+        
+        if player1.y <= 0:
+            player1.y = 0
+        elif player1.y >= 720 - 150:
+            player1.y = 720 - 150
+
+        player1.y += player1_speed
+
+
+        if ball.x <= 0:
+            player2_score += 1
+            placar_player2 = font.render(str(player2_score), True, "white")
+            ball.x = 600
+            ball_dir_x *= -1
+        elif ball.x >= 1280:
+            player1_score += 1
+            placar_player1 = font.render(str(player1_score), True, "white")
+            ball.x = 600
+            ball_dir_x *= -1
+
+        if ball.y <= 0 or ball.y >= (720 - 15):
+            ball_dir_y *= -1
+
+        ball.x += ball_dir_x 
+        ball.y += ball_dir_y 
+
+        player2.y = ball.y - 75
+
+        if player2.y <= 0:
+            player2.y = 0
+        elif player2.y >= 720 - 150:
+            player2.y = 720 - 150
+
+        display.fill((0,0,0))
+        display.blit(campo_img, campo)
+        display.blit(p1_img, player1)
+        display.blit(p2_img, player2)
+        display.blit(ball_img, ball)
+        display.blit(placar_player1, (500,50))
+        display.blit(placar_player2, (780,50))
+
+    elif cena == "gameover":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    loop = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    cena = "menu"
+
+        display.fill((0,0,0))
+        text_win = font.render("Game Over", True, "red")
+        display.blit(text_win, [(540), 360])
+
+    elif cena == "menu":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    loop = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    player1_score = 0
+                    player2_score = 0
+                    placar_player1 = font.render(str(player1_score), True, "white")
+                    placar_player2 = font.render(str(player2_score), True, "white")
+                    cena = "jogo"
+                    fade_alpha = 255
+                    
+                    start = pygame.mixer.Sound("assets/start.wav")
+                    start.play()
+
+        fade_alpha -= 1
+        fade_img.set_alpha(fade_alpha)
+
+        display.fill((0,0,0))
+        title = font.render("Pong Pong", True, "red")
+        text = font.render("Precione start para iniciar", True, "white")
+        display.blit(title, [(540), 360])    
+        display.blit(text, [(440), 460])    
+        display.blit(fade_img, [0, 0])
+    
+    fps.tick(60)
+    pygame.display.flip()
